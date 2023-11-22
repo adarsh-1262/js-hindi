ef70b7dfa9042328ab08de95625495a91ad7c823
ADARSH SINGH <9792051262a@gmail.com>
2023-11-22 11:50:49 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all JS code as newer versions
+
+// alert( 3 + 3) // we are using nodejs , not browser, so this cann't be run
+
+console.log( 3 + 3)
+
+// code readability should be high 
+
+console.log( "Adarsh")
+
+let name = " Adarsh "
+let age = 19
+let isLoggedIn = true
+
+// PRIMITIVE DATA TYPES:-
+
+// number =>  2 to power 53
+// bigint
+// string
+// boolean
+// null => Standalone value
+// undefined => value is not assigned till now
+// symbol => Uniqueness  (for finding uniqueness in the same kind of so many objects)
+
+// object
+console.log(typeof age)
+console.log(typeof undefined)     //undefined
+console.log(typeof null)          //object
