9beba06a4c9b3a58e1babf9eb5a0638d7fd8367e
ADARSH SINGH <9792051262a@gmail.com>
2023-11-23 14:10:36 +0000

    promise.js concepts

---
+++ 03_advance/promises.js
@@ -0,0 +1,57 @@
+const promiseOne = new Promise(function(resolve, reject) {
+    //Do any async task
+    // DB Calls, cryptography, network
+    setTimeout(function() {
+        console.log('Async task is complete');
+        resolve();
+    }, 10000)
+})
+
+promiseOne.then(function() {
+    console.log("Promise consumed");
+})
+
+new Promise(function(resolve, reject) {
+    setTimeout(function() {
+        console.log("Async task 2");
+        resolve();
+    }, 10000)
+}).then(function() {
+    console.log("Async 2 resolved");
+})
+
+const promiseThree = new Promise(function(resolve, reject) {
+    setTimeout(function(){
+        resolve({username: "Vipul", email: "adarshsinggh1262@gmail.com"})
+    }, 1000)
+})
+
+promiseThree.then(function(user){
+    console.log(user);
+})
+
+const promiseFourth = new Promise(function(resolve, reject){
+    setTimeout(function(){
+        let error = false;
+        if(!error){
+            resolve({username: "Vipul", password: "12345"});
+        }else{
+            reject('ERROR: Something went wrong');
+        }
+    }, 1000)
+})
+
+// promiseFourth.then().catch()
+promiseFourth
+.then((user) => {
+    console.log(user);
+    return user.username;
+})
+.then((username) => {
+    console.log(username);
+})
+.catch(function(error){
+    console.log(error);
+})
+.finally(() => console.log("The promise is either resolved or rejected"));
+
