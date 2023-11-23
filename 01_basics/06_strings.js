900ec87376cb2f179d4bd4148c2dba2a6ecdd376
ADARSH SINGH <9792051262a@gmail.com>
2023-11-23 10:04:12 +0000

    number and maths

---
+++ 01_basics/06_strings.js
@@ -0,0 +1,17 @@
+const name = "adarsh";
+const repoCount = 22 ;
+
+// console.log(name + repoCount + " value");
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gamename = new String("adarshss");
+
+console.log(gamename[0]);
+console.log(gamename.__proto__);
+
+console.log(gamename.length);
+console.log(gamename.toUpperCase());
+
+console.log(gamename.charAt(2));
+console.log(gamename.indexOf('s'));
